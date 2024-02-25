bcf4086f88f9a05898f1de8d7819e40fc96ac88a
FAQ9x <30382626+FAQ9x@users.noreply.github.com>
2024-02-25 10:47:35 +0700

    Update imglazyload.js

--- imglazyload.js
+++ imglazyload.js
@@ -1,1 +1,121 @@
-!function(a,b){"use strict";var c=function(a,b){if(!b)return a;var c;return function(){clearTimeout(c),c=setTimeout(function(){a()},b)}};a.fn.imgLazyLoad=function(d){var e=this,f=a.extend({container:b,effect:"fadeIn",speed:600,delay:400,callback:function(){}},d),g=a(f.container),h=function(){if(!e.length)return g.off("scroll.lazyLoad");var c=g.outerHeight(),d=g.scrollTop();f.container!==b&&(d=g.offset().top),e.each(function(){var b,g=a(this),h=g.offset().top;d+c>h&&h+g.outerHeight()>d&&(e=e.not(g),b=g.attr("data-src"),a(new Image).prop("src",b).load(function(){g.hide().attr("src",b)[f.effect](f.speed,function(){f.callback.call(this)}).removeAttr("data-src")}))})};if(!g.length)throw f.container+" is not defined";return h(),g.on("scroll.imgLazyLoad",c(h,f.delay)),this}}(jQuery,window);
+;(function( $, win ){
+
+	'use strict';
+
+	var throttle = function( fn, delay ){
+
+		if( !delay ){
+
+			return fn;
+
+		}
+
+		var timer;
+
+		return function(){
+
+			clearTimeout( timer );
+
+			timer = setTimeout(function(){
+
+				fn();
+
+			}, delay );
+
+		}
+
+	};
+
+	$.fn.imgLazyLoad = function( options ){
+
+		var elements = this,
+			settings = $.extend({
+
+				container: win,
+				effect: 'fadeIn',
+				speed: 600,
+				delay: 400,
+				callback: function(){}
+
+			}, options ),
+
+			container = $( settings.container ),
+
+			loading = function(){
+
+				//当所有的图片都加载完，移除滚动事件
+				if( !elements.length ){
+
+					return container.off( 'scroll.lazyLoad' );
+
+				}
+
+				var containerHeight = container.outerHeight(),
+					containerTop = container.scrollTop();
+
+				if( settings.container !== win ){
+
+					containerTop = container.offset().top;
+
+				}
+
+				elements.each(function(){
+
+					var $this = $( this ),
+						top = $this.offset().top;
+					
+					if( containerTop + containerHeight > top &&
+						top + $this.outerHeight() > containerTop ){
+
+						//删除jQuery选择好的元素集合中已经被加载的图片元素
+						elements = elements.not( $this );
+						
+						var loadingSrc = $this.attr( 'data-src' );
+						
+						$( new Image() ).prop( 'src', loadingSrc ).load(function(){
+
+							//替换图片路径并执行特效
+							$this.hide()
+								.attr( 'src', loadingSrc )
+								[ settings.effect ]( settings.speed, function(){
+
+									settings.callback.call( this );
+
+								})
+								.removeAttr( 'data-src' );
+
+						});
+
+					}
+
+				});
+			};
+
+		if( !container.length ){
+
+			throw settings.container + ' is not defined';
+
+		}
+
+		//开始便加载已经出现在可视区的图片
+		loading();
+
+		//滚动监听，显示图片
+		container.on( 'scroll.imgLazyLoad', throttle( loading, settings.delay ) );
+
+		return this;
+	};
+	
+})( jQuery, window );
+			$('.container img').imgLazyLoad({
+				// jquery selector or JS object
+				container: window,
+				// jQuery animations: fadeIn, show, slideDown
+				effect: 'fadeIn',
+				// animation speed
+				speed: 600,
+				// animation delay
+				delay: 400,
+				// callback function
+				callback: function(){}
+			});
