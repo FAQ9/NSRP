497d5bb83a02f20b9c4d4c04f589b1a76d228742
FAQ9x <30382626+FAQ9x@users.noreply.github.com>
2024-02-25 10:51:39 +0700

    Update imglazyload.js

--- imglazyload.js
+++ imglazyload.js
@@ -107,15 +107,4 @@
 	};
 	
 })( jQuery, window );
-			$('.movie-item img').imgLazyLoad({
-				// jquery selector or JS object
-				container: window,
-				// jQuery animations: fadeIn, show, slideDown
-				effect: 'fadeIn',
-				// animation speed
-				speed: 600,
-				// animation delay
-				delay: 400,
-				// callback function
-				callback: function(){}
-			});
+			
